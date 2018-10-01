d2fc90f80c8db7e4ee594d9f5e0b24affa30b488
NoUser123098 <31293075+NoUser123098@users.noreply.github.com>
2018-10-01 08:40:01 -0500

    Update simple.js

--- simple.js
+++ simple.js
@@ -2,7 +2,7 @@ $(function(){
   $("#numGen").click(function(){
     var a = prompt("Enter 1st Number.");
     var b = prompt("Enter 2nd Number.");
-    numGen(a, b);
+    $("#number").html(numGen(a, b));
   });
 });
 function numGen(a, b){
