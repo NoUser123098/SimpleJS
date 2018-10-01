bf62f661dd54113e899911a42e2ff3619045a34e
NoUser123098 <31293075+NoUser123098@users.noreply.github.com>
2018-10-01 08:33:01 -0500

    Update simple.js

--- simple.js
+++ simple.js
@@ -1,3 +1,12 @@
 $(function(){
-  
+  $("#numGen").click(function(){
+    var a = $("#gen1").val();
+    var b = $("#gen2").val();
+    numGen(a, b);
+  });
 });
+function numGen(a, b){
+  a = Math.ceil(a);
+  b = Math.floor(b);
+  return Math.floor(Math.random() * (b - a)) + a;
+}
